2fe971a258a9912fa1fdb0873db8da59d5b12d5d
Khải Võ <kenkaneki395@gmail.com>
2026-05-15 16:26:10 +0700

    feat: integrate Hardcodet TaskbarIcon
    - Build: Add `Hardcodet.NotifyIcon.Wpf` to `.csproj` as the primary library for System Tray integration.
    - Fix(UI): Remove unstable `Wpf.Ui.Tray` XAML declarations in `MainWindow.xaml` to resolve "InitializeComponent" compilation errors.
    - Refactor: Initialize `TaskbarIcon` entirely via C# code-behind in `MainWindow.xaml.cs` for type safety and stability.

--- MyDeusTools.App/MainWindow.xaml.cs
+++ MyDeusTools.App/MainWindow.xaml.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Windows;
 using Wpf.Ui.Controls;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace MyDeusTools.App;
 
 public partial class MainWindow : FluentWindow
 {
     public ViewModels.MainWindowViewModel ViewModel { get; }
+    private TaskbarIcon? _notifyIcon;
+    private bool _isExitAllowed = false;
 
     public MainWindow(ViewModels.MainWindowViewModel viewModel, Wpf.Ui.INavigationService navigationService)
     {
@@ -17,6 +20,56 @@ public partial class MainWindow : FluentWindow
 
         // Cấu hình điều hướng: Gán NavigationView cho Service
         navigationService.SetNavigationControl(RootNavigation);
+
+        // Cấu hình Tray
+        InitializeTray();
+        Closing += MainWindow_Closing;
+    }
+
+    private void InitializeTray()
+    {
+        _notifyIcon = new TaskbarIcon
+        {
+            ToolTipText = "MyDeusTools is running",
+            IconSource = new System.Windows.Media.Imaging.BitmapImage(new Uri("pack://application:,,,/Resources/avatar.ico")),
+            ContextMenu = new System.Windows.Controls.ContextMenu()
+        };
+
+        var openItem = new System.Windows.Controls.MenuItem { Header = "Mở ứng dụng" };
+        openItem.Click += OnShowMainWindow;
+
+        var exitItem = new System.Windows.Controls.MenuItem { Header = "Thoát" };
+        exitItem.Click += OnExitApp;
+
+        _notifyIcon.ContextMenu.Items.Add(openItem);
+        _notifyIcon.ContextMenu.Items.Add(new System.Windows.Controls.Separator());
+        _notifyIcon.ContextMenu.Items.Add(exitItem);
+
+        _notifyIcon.TrayLeftMouseUp += (s, e) => ShowWindow();
+    }
+
+    private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+    {
+        if (!_isExitAllowed)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+
+    private void OnShowMainWindow(object? sender, RoutedEventArgs e) => ShowWindow();
+
+    private void OnExitApp(object? sender, RoutedEventArgs e)
+    {
+        _isExitAllowed = true;
+        Application.Current.Shutdown();
+    }
+
+    private void ShowWindow()
+    {
+        this.Show();
+        this.WindowState = WindowState.Normal;
+        this.Activate();
     }
 
     private void OnThemeToggleClick(object sender, RoutedEventArgs e)
